f19f103db195db76e3d3a2f8c29744f0fc28e7f6
to-som-ja <davidkoso252123@gmail.com>
2021-12-27 13:20:55 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Client.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Client.dir/main.c.obj"
+  "Client.exe"
+  "Client.exe.manifest"
+  "Client.lib"
+  "Client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, asasasaWorld!\n");
+    return 0;
+}
