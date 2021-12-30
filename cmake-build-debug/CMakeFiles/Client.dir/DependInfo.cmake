0e4e27b339feb9f4ec29f7cc56c8af1221993bba
Matej Suchý <matejsuchy@studentambassadors.com>
2021-12-30 18:53:04 +0100

    Vytvorenie zakladnej logiky menu.

--- cmake-build-debug/CMakeFiles/Client.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Client.dir/DependInfo.cmake
@@ -1,23 +1,15 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
   "C"
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/Lenovo/Desktop/Client/main.c" "C:/Users/Lenovo/Desktop/Client/cmake-build-debug/CMakeFiles/Client.dir/main.c.obj"
+  "/tmp/tmp.9GFpQ2mIlJ/main.c" "/tmp/tmp.9GFpQ2mIlJ/cmake-build-debug/CMakeFiles/Client.dir/main.c.o"
   )
-set(CMAKE_C_COMPILER_ID "MSVC")
+set(CMAKE_C_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
-  )
-
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
   )
 
 # Targets to which this target links.
--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 #define X 30 //stlpce
 #define Y 10 //riadok
 #define POCET 10
+#define POCET_AKCII 12
+
 typedef struct Mravec {
     int polohaX;
     int polohaY;
@@ -25,6 +27,7 @@ typedef struct data {
     int pocetM;
     Mravec *zoznamMravcov;
     int vykresluje;
+    int akcieStret;
     pthread_mutex_t *mutex;
     pthread_cond_t *vypocitane;
     pthread_cond_t *vykreslene;
@@ -66,48 +69,85 @@ void posunMravca (int maxX, int maxY, Mravec *m) {
 void *logika(void *data) {
     printf("Zacinam pocitat\n");
     DATA *dataV = (DATA *) data;
-    pthread_mutex_lock(dataV->mutex);
-    while (dataV->vykresluje) {
-        pthread_cond_wait(dataV->vypocitane, dataV->mutex);
-    }
-    for (int i = 0; i < dataV->pocetM; ++i) {
-        Mravec *mravec = &dataV->zoznamMravcov[i];
-        int polohaX = mravec->polohaX;
-        int polohaY = mravec->polohaY;
-        int *farbaPolicka = &dataV->pPlocha->plocha[mapFunction(polohaX, polohaY)];
+    while (1) {
+        pthread_mutex_lock(dataV->mutex);
+        while (dataV->vykresluje) {
+            pthread_cond_wait(dataV->vypocitane, dataV->mutex);
+        }
+        for (int i = 0; i < dataV->pocetM; ++i) {
+            Mravec *mravec = &dataV->zoznamMravcov[i];
+            int polohaX = mravec->polohaX;
+            int polohaY = mravec->polohaY;
+            int *farbaPolicka = &dataV->pPlocha->plocha[mapFunction(polohaX, polohaY)];
+/* TODO
+ * akcia pri strete viacerych mravcov 0/1/2
+ */
+            // inverzna logika
+            if (mravec->logika) {
+                // cierne policko
+                if (*farbaPolicka) {
+                    mravec->smer = (mravec->smer + 1) % 4;
+                    *farbaPolicka = 1;
+                }
+                    // biele policko
+                else {
+                    mravec->smer = (mravec->smer + 3) % 4;
+                    *farbaPolicka = 0;
+                }
+            }
+                // priama logika
+            else {
+                // biele policko
+                if (!(*farbaPolicka)) {
+                    mravec->smer = (mravec->smer + 1) % 4;
+                    *farbaPolicka = 1;
+                }
+                    // cierne policko
+                else {
+                    mravec->smer = (mravec->smer + 3) % 4;
+                    *farbaPolicka = 0;
+                }
+            }
+            posunMravca(*dataV->pPlocha->velkostX, *dataV->pPlocha->velkostY, mravec);
+            int cisloPrveho;
+            int cisloDruheho;
+            int nachadzaSaInyMravec=0;
+            for (int j = 0; j < dataV->pocetM; ++j) {
+                if(dataV->zoznamMravcov[i].polohaX==polohaX && dataV->zoznamMravcov[i].polohaY==polohaY)
+                {
+                    if (nachadzaSaInyMravec==0){
+                        cisloPrveho=j;
+                    }else{
+                        cisloDruheho = j;
+                    }
+                    nachadzaSaInyMravec++;
+                }
 
-        // inverzna logika
-        if(mravec->logika) {
-            // cierne policko
-            if (*farbaPolicka) {
-                mravec->smer = (mravec->smer + 1) % 4;
-                *farbaPolicka = 1;
             }
-            // biele policko
-            else {
-                mravec->smer = (mravec->smer + 3) % 4;
-                *farbaPolicka = 0;
-            }
-        }
-        // priama logika
-        else {
-            // biele policko
-            if (!(*farbaPolicka)) {
-                mravec->smer = (mravec->smer + 1) % 4;
-                *farbaPolicka = 1;
-            }
-            // cierne policko
-            else {
-                mravec->smer = (mravec->smer + 3) % 4;
-                *farbaPolicka = 0;
+            if(nachadzaSaInyMravec>1){
+                switch (dataV->akcieStret) {
+                    case 0:
+                        dataV->zoznamMravcov[cisloDruheho]=dataV->zoznamMravcov[dataV->pocetM];
+                        dataV->pocetM--;
+                        dataV->zoznamMravcov[cisloPrveho]=dataV->zoznamMravcov[dataV->pocetM];
+                        dataV->pocetM--;
+                        break;
+                    case 1:
+                        //memmove(&dataV->zoznamMravcov[cisloDruheho],&dataV->zoznamMravcov[dataV->pocetM],sizeof (Mravec));
+                        dataV->zoznamMravcov[cisloDruheho]=dataV->zoznamMravcov[dataV->pocetM];
+                            dataV->pocetM--;
+                        break;
+                    case 2:
+
+                        break;
+                }
             }
         }
-        posunMravca(*dataV->pPlocha->velkostX, *dataV->pPlocha->velkostY, mravec);
+        dataV->vykresluje = 1;
+        pthread_cond_signal(dataV->vykreslene);
+        printf("Koncim pocitanie\n");
+        pthread_mutex_unlock(dataV->mutex);
     }
-    dataV->vykresluje = 1;
-    pthread_cond_signal(dataV->vykreslene);
-    printf("Koncim pocitanie\n");
-    pthread_mutex_unlock(dataV->mutex);
     return 0;
 }
 
@@ -143,71 +183,88 @@ void nastavCierne (Plocha *p) {
 
 void *zobraz(void *data) {
     DATA *dataV = (DATA *) data;
-    pthread_mutex_lock(dataV->mutex);
-    while (!dataV->vykresluje) {
-        pthread_cond_wait(dataV->vykreslene, dataV->mutex);
-    }
-    printf("Zobrazujem\n");
-    for (int i = 0; i < *dataV->pPlocha->velkostY; ++i) {
-        for (int j = 0; j < *dataV->pPlocha->velkostX; ++j) {
-            int smerMravca = 4;         // 4 - na policku nie je mravec
-
-            for (int k = 0; k < dataV->pocetM; ++k) {
-                if (dataV->zoznamMravcov[k].polohaX == j && dataV->zoznamMravcov[k].polohaY == i) {
-                    smerMravca = dataV->zoznamMravcov[k].smer;
-
-                }
-            }
-
-            int farbaPolicka = dataV->pPlocha->plocha[mapFunction(j,i)];
-            if (smerMravca != 4) {
-                switch (smerMravca) {
-                    case 0:
-                        if (farbaPolicka) {
-                            printf("▲ ");
-                        } else {
-                            printf("^ ");
-                        }
-                        break;
-                    case 1:
-                        if (farbaPolicka) {
-                            printf("► ");
-                        } else {
-                            printf("> ");
-                        }
-                        break;
-                    case 2:
-                        if (farbaPolicka) {
-                            printf("▼ ");
-                        } else {
-                            printf("v ");
-                        }
-                        break;
-                    case 3:
-                        if (farbaPolicka) {
-                            printf("◄ ");
-                        } else {
-                            printf("< ");
-                        }
-                        break;
-                }
-            } else {
-                if (farbaPolicka == 0) {
-                    printf("□ ");
-                } else {
-                    printf("■ ");
-                }
-            }
-
+    while (1) {
+        pthread_mutex_lock(dataV->mutex);
+        while (!dataV->vykresluje) {
+            pthread_cond_wait(dataV->vykreslene, dataV->mutex);
         }
-        printf("\n");
+        printf("Zobrazujem\n");
+        for (int i = 0; i < *dataV->pPlocha->velkostY; ++i) {
+            for (int j = 0; j < *dataV->pPlocha->velkostX; ++j) {
+                int smerMravca = 4;         // 4 - na policku nie je mravec
+
+                for (int k = 0; k < dataV->pocetM; ++k) {
+                    if (dataV->zoznamMravcov[k].polohaX == j && dataV->zoznamMravcov[k].polohaY == i) {
+                        smerMravca = dataV->zoznamMravcov[k].smer;
+
+                    }
+                }
+
+                int farbaPolicka = dataV->pPlocha->plocha[mapFunction(j, i)];
+                if (smerMravca != 4) {
+                    switch (smerMravca) {
+                        case 0:
+                            if (farbaPolicka) {
+                                printf("▲ ");
+                            } else {
+                                printf("^ ");
+                            }
+                            break;
+                        case 1:
+                            if (farbaPolicka) {
+                                printf("► ");
+                            } else {
+                                printf("> ");
+                            }
+                            break;
+                        case 2:
+                            if (farbaPolicka) {
+                                printf("▼ ");
+                            } else {
+                                printf("v ");
+                            }
+                            break;
+                        case 3:
+                            if (farbaPolicka) {
+                                printf("◄ ");
+                            } else {
+                                printf("< ");
+                            }
+                            break;
+                    }
+                } else {
+                    if (farbaPolicka == 0) {
+                        printf("□ ");
+                    } else {
+                        printf("■ ");
+                    }
+                }
+
+            }
+            printf("\n");
+        }
+        dataV->vykresluje = 0;
+        pthread_cond_signal(dataV->vypocitane);
+        printf("Koncim zobrazovanie\n");
+        pthread_mutex_unlock(dataV->mutex);
     }
-    dataV->vykresluje = 0;
-    pthread_cond_signal(dataV->vypocitane);
-    printf("Koncim zobrazovanie\n");
-    pthread_mutex_unlock(dataV->mutex);
     return 0;
 }
+
+void vytvorSvet(Plocha *pPlocha) {
+    int rozmerX, rozmerY;
+    printf("Zadajte X rozmer sveta: ");
+    scanf("%d", &rozmerX);
+    printf("\nZadajte Y rozmer sveta: ");
+    scanf("%d", &rozmerY);
+    printf("\n");
+
+    pPlocha->velkostY = &rozmerY;
+    pPlocha->velkostX = &rozmerX;
+    pPlocha->plocha = malloc(sizeof(int) * rozmerX * rozmerY);
+    memset(pPlocha->plocha, 0, rozmerX * rozmerY * sizeof(int));
+}
+
 
 // TODO
 /*
@@ -216,13 +273,12 @@ void *zobraz(void *data) {
  */
 int main() {
     srand(time(NULL));
-    // TODO
-    /*
-     * Sparametrizovat
-     */
-    int velkostX = X;
-    int velkostY = Y;
-    int pocet = POCET;
+    char vstup;
+    Plocha p;
+    int pocet;
+    DATA d;
+    d.akcieStret = 0;
+
     int vykresluje = 1;
     pthread_t grafikaTH, logikaTH;
     pthread_mutex_t mutex;
@@ -232,32 +288,124 @@ int main() {
     pthread_cond_init(&vykreslene, NULL);
     pthread_cond_init(&vypocitane, NULL);
 
-    Plocha p = {&velkostX, &velkostY, malloc(sizeof(int) * velkostX * velkostY)};
-    memset(p.plocha, 0, velkostX * velkostY * sizeof(int));
-//    nahodneCierne(&p);
-    nastavCierne(&p);
 
-    DATA d = {&p, pocet, malloc(sizeof(Mravec) * pocet), vykresluje, &mutex, &vykreslene, &vypocitane};
+    char textAkcii[POCET_AKCII][36] = {"1 - Vytvorit svet", "2 - Generovat cierne polia", "3 - Definovat konkretne cierne polia",
+                                       "4 - Nacitat svet (lokalne)", "5 - Ulozit svet (lokalne)", "6 - Pocet mravcov", "7 - Logika mravcov",
+                                       "8 - Akcia pri strete mravcov", "9 - Spustit / Zastavit simulaciu", "10 - Nacitat svet (server)",
+                                       "11 - Ulozit svet (server)", "X - Ukoncit aplikaciu"};
+    int dostupneAkcie[POCET_AKCII] = {1,0,0,1,0,0,0,0,0,1,0,1}; //bitova reprezentacia dostupnych akcii
+    while (strcmp(vstup, "X") != 0) {
+        printf("---MENU---\nMozne akcie:\n");
+        for (int i = 0; i < POCET_AKCII; ++i) {
+            if(dostupneAkcie[i]) {
+                printf("%s", textAkcii[i]);
+                printf("\n");
+            }
+        }
+        printf("Vyberam akciu cislo: ");
+        scanf("%c", vstup);
+        if (strcmp(vstup, "X") == 0) {
+            ukonciAplikaciu();
+        }
+        int akcia = atoi(&vstup);
+        switch (akcia) {
+            /* TODO
+             * Skontrolovat bitovu reprezentaciu pri kazdom case!!!
+             */
+            case 1:
+                if (dostupneAkcie[0]) {
+                    vytvorSvet(&p);
+                    int dostupneAkcie2[12] = {0,1,1,0,1,1,0,0,0,0,1,1};
+                    memcpy(dostupneAkcie, dostupneAkcie2, sizeof (dostupneAkcie));
 
-    for (int i = 0; i < pocet; ++i) {
-        int polohaX = rand() % velkostX;
-        int polohaY = rand() % velkostY;
-        int smer = rand() % 4;
+                }
+                break;
+            case 2:
+                if (dostupneAkcie[1]) {
+                    nahodneCierne(&p);
+                }
+                break;
+            case 3:
+                if (dostupneAkcie[2]) {
+                    nastavCierne(&p);
+                }
+                break;
+            case 4:
+                if (dostupneAkcie[3]) {
+                    // nacitajSvetLokalne(&p);
+                }
+                break;
+            case 5:
+                if (dostupneAkcie[4]) {
+                    // ulozSvetLokalne(&p);
+                }
+                break;
+            case 6:
+                if (dostupneAkcie[5]) {
+                    printf("Zadajte pocet mravcov: ");
+                    scanf("%d", &pocet);
+                    d.pocetM = pocet;
+                    d.zoznamMravcov = malloc(sizeof(Mravec) * pocet);
+                    for (int i = 0; i < pocet; ++i) {
+                        int polohaX = rand() % *p.velkostX;
+                        int polohaY = rand() % *p.velkostY;
+                        int smer = rand() % 4;
 
-        int logika = 0;
-        Mravec m = {polohaX, polohaY, smer, logika};
-        d.zoznamMravcov[i] = m;
+                        int logika = 0;
+                        Mravec m = {polohaX, polohaY, smer, logika};
+                        d.zoznamMravcov[i] = m;
+                    }
+                }
+                break;
+            case 7:
+                if (dostupneAkcie[6]) {
+                    int logika;
+                    printf("Vyberte logiku mravcov (0 = priama / 1 = inverzna): ");
+                    scanf("%d", &logika);
+                    for (int i = 0; i < pocet; ++i) {
+                        d.zoznamMravcov[i].logika = logika;
+                    }
+                }
+                break;
+            case 8:
+                if (dostupneAkcie[7]) {
+                    int akciaStret;
+                    printf("Vyberte ako sa budu mravce spravat pri strete (0 = zanik vsetkych stretnutych mravcov / 1 = prezije jeden mravec / 2 = polovica mravcov sa zacne spravat podla doplnkovej logiky): ");
+                    scanf("%d", &akciaStret);
+                    d.akcieStret = akciaStret;
+                }
+                break;
+            case 9:
+                if (dostupneAkcie[8]) {
+                    zapniVypni();
+                }
+                break;
+            case 10:
+                if (dostupneAkcie[9]) {
+                    nacitajSvetZoServera();
+                }
+                break;
+            case 11:
+                if (dostupneAkcie[10]) {
+                    ulozSvetNaServer();
+                }
+                break;
+        }
+        d.pPlocha = &p;
+        //  = {&p, pocet, malloc(sizeof(Mravec) * pocet), vykresluje, &mutex, &vykreslene, &vypocitane}
     }
+
+
     pthread_create(&grafikaTH, NULL, &zobraz, &d);
     pthread_create(&logikaTH, NULL, &logika, &d);
 
     pthread_join(grafikaTH, NULL);
     pthread_join(logikaTH, NULL);
 
-
     pthread_mutex_destroy(&mutex);
     pthread_cond_destroy(&vypocitane);
     pthread_cond_destroy(&vykreslene);
     return 0;
 }
+
 
